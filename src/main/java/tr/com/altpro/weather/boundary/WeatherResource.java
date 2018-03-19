c8b247581b6538499cab8a12951f2418f8979f13
aygunbozdemir <aygun.bozdemir@altpro.com.tr>
2018-03-19 17:57:47 +0300

    restTemplate exception handling will be added

--- src/main/java/tr/com/altpro/weather/boundary/WeatherResource.java
+++ src/main/java/tr/com/altpro/weather/boundary/WeatherResource.java
@@ -1,11 +1,13 @@
 package tr.com.altpro.weather.boundary;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import tr.com.altpro.weather.controller.WeatherService;
+import tr.com.altpro.weather.entity.WeatherException;
 import tr.com.altpro.weather.entity.WeatherResponse;
 
 import javax.inject.Inject;
@@ -19,10 +21,16 @@ public class WeatherResource {
 
     @RequestMapping(value = "/getByCityName", method = RequestMethod.GET)
     public ResponseEntity<WeatherResponse> getWeatherByCityName(@RequestParam(value="cityName",required = true) String cityName) {
-        if(cityName != null && !cityName.isEmpty()) {
+        ResponseEntity<WeatherResponse> responseEntity = weatherService.getWeatherByCityName(cityName); // TODO : add exception handling to REstTemplate
+        if(responseEntity.equals(HttpStatus.NOT_FOUND)) {
+            throw new WeatherException("City not found");
+        }
+        else if(cityName != null && !cityName.isEmpty()) {
             return weatherService.getWeatherByCityName(cityName);
         }
-        return null;
+        else {
+            throw new WeatherException("City name parameter should not be null");
+        }
     }
     // TODO getWeatherByLocaiton logic
 }

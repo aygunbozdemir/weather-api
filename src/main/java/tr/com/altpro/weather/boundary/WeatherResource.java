351e062896ee1d4b5087b38da1f7d64cc865cb3d
aygunbozdemir <aygun.bozdemir@altpro.com.tr>
2018-03-19 15:03:58 +0300

    service and controller added and lombox errors fixed

--- src/main/java/tr/com/altpro/weather/boundary/WeatherResource.java
+++ src/main/java/tr/com/altpro/weather/boundary/WeatherResource.java
@@ -1,4 +1,27 @@
 package tr.com.altpro.weather.boundary;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import tr.com.altpro.weather.controller.WeatherService;
+import tr.com.altpro.weather.entity.WeatherResponse;
+
+import javax.inject.Inject;
+
+@RestController
+@RequestMapping(value = "/weather/getWeather")
 public class WeatherResource {
+
+    @Inject
+    WeatherService weatherService;
+
+    @RequestMapping(value = "/getByCityName", method = RequestMethod.GET)
+    public ResponseEntity<WeatherResponse> getWeatherByCityName(@RequestParam(value="cityName",required = true) String cityName) {
+        if(cityName != null && !cityName.isEmpty()) {
+            return weatherService.getWeatherByCityName(cityName);
+        }
+        return null;
+    }
 }
--- src/main/java/tr/com/altpro/weather/controller/WeatherService.java
+++ src/main/java/tr/com/altpro/weather/controller/WeatherService.java
@@ -1,8 +1,23 @@
 package tr.com.altpro.weather.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import tr.com.altpro.weather.entity.WeatherResponse;
+
+import javax.inject.Inject;
 
 @Service
 public class WeatherService {
 
+    @Inject
+    private RestTemplate restTemplate;
+
+    public ResponseEntity<WeatherResponse> getWeatherByCityName(String cityName) {
+
+        WeatherResponse response = restTemplate.getForObject("api.openweathermap.org/data/2.5/weather?q="+cityName,WeatherResponse.class);
+        return new ResponseEntity<>(response, HttpStatus.FOUND);
+    }
 }

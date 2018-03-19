b5d31ffd6eb8b9a0640723812f78701959bca5d4
aygunbozdemir <aygun.bozdemir@altpro.com.tr>
2018-03-19 16:50:38 +0300

    api key added

--- src/main/java/tr/com/altpro/weather/controller/WeatherService.java
+++ src/main/java/tr/com/altpro/weather/controller/WeatherService.java
@@ -16,7 +16,7 @@ public class WeatherService {
     private RestTemplate restTemplate;
 
     public ResponseEntity<WeatherResponse> getWeatherByCityName(String cityName) {
-        WeatherResponse response = restTemplate.getForObject("http://samples.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=b6907d289e10d714a6e88b30761fae22", WeatherResponse.class);
+        WeatherResponse response = restTemplate.getForObject("http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=2307c4fb0bedb1b9f1bcbbe8e91814d1", WeatherResponse.class);
         return new ResponseEntity<>(response, HttpStatus.FOUND);
     }
 }
